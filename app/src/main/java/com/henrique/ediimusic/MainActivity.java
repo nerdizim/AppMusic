31a1977f1fcd880c815545982a4d363639a95554
nerdizim <cassio.henrique14@gmail.com>
2022-03-18 16:47:51 -0300

    First commit

---
+++ app/src/main/java/com/henrique/ediimusic/MainActivity.java
@@ -0,0 +1,16 @@
+package com.henrique.ediimusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
